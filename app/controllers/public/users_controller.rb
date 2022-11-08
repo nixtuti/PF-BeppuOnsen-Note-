c512b31cf937dda522a12b4bc81e0868b60bf318
EC2 Default User <111040404+nixtuti@users.noreply.github.com>
2022-11-08 15:45:02 +0000

    [Fix]ゲストログイン

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,5 +1,5 @@
 class Public::UsersController < ApplicationController
-  before_action :ensure_normal_user, only: :update
+  before_action :ensure_guest_user, only: [:edit]
   
   def show
     @user = User.find(params[:id])
@@ -31,9 +31,11 @@ class Public::UsersController < ApplicationController
     params.require(:user).permit(:username, :sex, :birth_date, :email, :status, :profile_image)
   end
   
-  def ensure_normal_user
-    if current_user.email == 'guest@example.com'
-      redirect_to root_path, alert: 'ゲストユーザーは更新できません。'
+  def ensure_guest_user
+    @user = User.find(params[:id])
+    if @user.username == "ゲスト"
+      redirect_to user_path(current_user) , notice: 'ゲストユーザーはプロフィール編集画面へ遷移できません。'
     end
-  end
+  end  
+    
 end
