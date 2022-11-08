d7531aa47429823e8d898a4460e2ccd4c1ba11a6
EC2 Default User <111040404+nixtuti@users.noreply.github.com>
2022-11-08 10:17:52 +0000

    [Add]ユーザー詳細画面

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,16 +1,26 @@
 class Public::UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+    user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: "You have updated user successfully."
+    else
+      render :edit
+    end
   end
 
   def withdrawal_confirm
+    
   end
 
   def withdrawal
+    
   end
 end
