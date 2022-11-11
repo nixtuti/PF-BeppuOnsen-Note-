6b6dff01e2fbc8367628b0c8940640986b8f95a9
EC2 Default User <111040404+nixtuti@users.noreply.github.com>
2022-11-11 05:39:01 +0000

    [Add]温泉クチコミ編集、削除

--- app/controllers/public/reviews_controller.rb
+++ app/controllers/public/reviews_controller.rb
@@ -3,7 +3,7 @@ class Public::ReviewsController < ApplicationController
   def index
     @hot_spring = HotSpring.find(params[:hot_spring_id])
     @review = @hot_spring.reviews.new
-    @reviews = @hot_spring.reviews.page(params[:page])
+    @reviews = @hot_spring.reviews.page(params[:page]).order(created_at: :desc)
   end
 
   def create
@@ -19,20 +19,29 @@ class Public::ReviewsController < ApplicationController
   end
   
   def show
-    @review = Review.find_by(params[:review_id])
+    @review = Review.find(params[:id])
   end
 
   def edit
     @review = Review.find(params[:id])
-    if @review.update(review_params)
-      redirect_to 'index', notice: "クチコミの編集に成功しました"
-    else
-      render 'index'
-    end
   end
   
   def update
+    @review = Review.find(params[:id])
+    if @review.update(review_params)
+      redirect_to hot_spring_review_path(@review), notice: "クチコミの編集に成功しました"
+    else
+      render 'edit'
+    end
+  end
   
+  def destroy
+    @review = Review.find(params[:id])
+    if @review.destroy
+      redirect_to hot_spring_reviews_path, notice: "クチコミを削除しました"
+    else
+      render 'index'
+    end
   end
   
   private
