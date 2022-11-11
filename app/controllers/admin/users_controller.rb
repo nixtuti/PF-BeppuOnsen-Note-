d59e3e2403c1e758307bfc896d4df06018920cc7
EC2 Default User <111040404+nixtuti@users.noreply.github.com>
2022-11-11 08:14:16 +0000

    [Add]管理者ユーザー一覧、編集

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,28 @@
+class Admin::UsersController < ApplicationController
+  def index
+    @users = User.page(params[:page])
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to admin_user_path(@user), notice: "ユーザー情報を編集しました"
+    else
+      render 'edit'
+    end
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:username, :sex, :birth_date, :email, :status, :profile_image)
+  end
+end
