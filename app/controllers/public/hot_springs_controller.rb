348594f777b08c44d4292b5c618324a3142c922e
EC2 Default User <111040404+nixtuti@users.noreply.github.com>
2022-11-16 13:48:42 +0000

    [Add]チェックボックスで検索

--- app/controllers/public/hot_springs_controller.rb
+++ app/controllers/public/hot_springs_controller.rb
@@ -1,20 +1,23 @@
 class Public::HotSpringsController < ApplicationController
-  
+
   def index
     #@hot_springs = HotSpring.page(params[:page])
     if params[:search].present?
       hot_springs = HotSpring.looks(params[:search])
+    elsif params[:quality_ids].compact_blank.present?
+      hot_spring_ids = HotSpringQuality.where(quality_id: params[:quality_ids].compact_blank).pluck(:hot_spring_id).uniq
+      hot_springs = HotSpring.where(id: hot_spring_ids)
     elsif params[:quality_id].present?
       @quality = Quality.find(params[:quality_id])
-      hot_spring = @quality.hot_springs.order(created_at: :desc)
+      hot_springs = @quality.hot_springs.order(created_at: :desc)
     else
       hot_springs = HotSpring.all.order(created_at: :desc)
     end
     @hot_springs = Kaminari.paginate_array(hot_springs).page(params[:page]).per(10)
   end
-  
+
   def show
     @hot_spring = HotSpring.find(params[:id])
   end
-  
+
 end
--- app/models/quality.rb
+++ app/models/quality.rb
@@ -1,4 +1,4 @@
 class Quality < ApplicationRecord
   has_many :hot_spring_qualities, dependent: :destroy
-  has_many :hot_spring, through: :hot_spring_qualities
+  has_many :hot_springs, through: :hot_spring_qualities
 end
