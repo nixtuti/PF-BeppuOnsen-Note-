458dfa32675143edc8102da929018892cd58557a
EC2 Default User <111040404+nixtuti@users.noreply.github.com>
2022-11-17 02:20:56 +0000

    [Edit]ユーザートップ画面、サイドバー、hot_springコントローラー

--- app/controllers/public/hot_springs_controller.rb
+++ app/controllers/public/hot_springs_controller.rb
@@ -7,9 +7,10 @@ class Public::HotSpringsController < ApplicationController
     elsif params[:quality_ids].compact_blank.present?
       hot_spring_ids = HotSpringQuality.where(quality_id: params[:quality_ids].compact_blank).pluck(:hot_spring_id).uniq
       hot_springs = HotSpring.where(id: hot_spring_ids)
-    elsif params[:quality_id].present?
-      @quality = Quality.find(params[:quality_id])
-      hot_springs = @quality.hot_springs.order(created_at: :desc)
+    #リンクの時のコード。チェックボックスに変更済
+    #elsif params[:quality_id].present?
+      #@quality = Quality.find(params[:quality_id])
+      #hot_springs = @quality.hot_springs.order(created_at: :desc)
     else
       hot_springs = HotSpring.all.order(created_at: :desc)
     end
