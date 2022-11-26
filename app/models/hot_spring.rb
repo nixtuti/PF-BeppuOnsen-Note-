cb025e7166362cd793e8c7d9fb7591d9e323fa73
EC2 Default User <111040404+nixtuti@users.noreply.github.com>
2022-11-26 16:49:21 +0000

    [Fix]タグ検索後にレビュー件数順のランキングをしようとすると発生するActiveRecord::StatementInvalidエラーを解決

--- app/models/hot_spring.rb
+++ app/models/hot_spring.rb
@@ -33,7 +33,7 @@ class HotSpring < ApplicationRecord
   scope :latest, -> {order(created_at: :desc)}
   scope :oldest, -> {order(created_at: :asc)}
   scope :rate_avg, -> {eager_load(:reviews).group("hot_springs.id").order("avg(reviews.rate) desc")}
-  scope :review_amount, -> {eager_load(:reviews).group("hot_springs.id").order("count(hot_spring_id) desc")}
+  scope :review_amount, -> {eager_load(:reviews).group("hot_springs.id").order("count(reviews.hot_spring_id) desc")}
   
   def bookmarked_by?(user)
     bookmarks.exists?(user_id: user.id)
