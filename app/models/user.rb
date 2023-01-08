4d8fdf41875785adae7f8da372e49b37637c8720
EC2 Default User <111040404+nixtuti@users.noreply.github.com>
2023-01-08 14:39:44 +0000

    [Fix]user.rb バリデーション変更、user新規登録view 生年月日表記変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,6 @@ class User < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
   has_many :visited_marks, dependent: :destroy
 
-  validates :sex, presence: true
   validates :username, presence: true
 
   has_one_attached :profile_image
